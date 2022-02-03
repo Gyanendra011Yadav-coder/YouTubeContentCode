b5b3f59c2ed13bb2873aff5e8ac46144dd9561fe
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-02-03 22:32:27 +0530

    Created this class to solve another problem of Choclate distribution amoung m students.

---
+++ src/sevenDaysStrategySolutionLoveBabbarBhaiyaSheet/chocalateDistibutionProblemQuestionFour.java
@@ -0,0 +1,18 @@
+package sevenDaysStrategySolutionLoveBabbarBhaiyaSheet;
+
+/**
+ * @author Gyanendra Yadav
+ * @created 03/02/2022- 10:28 PM
+ * @project YouTubeContent
+ */
+public class chocalateDistibutionProblemQuestionFour {
+    public static void main(String[] args) {
+
+    }
+
+    private static int count(int []chocalatesPacket, int numberOfPackets, int numberOfStudents){
+        int min_Diffrence=0;
+
+        return min_Diffrence;
+    }
+}
